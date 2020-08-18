10cfdcb05debd954a3d03471f4c13da368200522
anickel101 <anson.nickel@gmail.com>
2020-08-18 14:50:45 -0400

    fixed User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
     has_many :scenarios, through: :user_scenarios
     has_many :spells, through: :user_spells
     has_many :items, through: :user_items
+    belongs_to :game
 
     def pick_item(item)
         UserItem.create(user_id: self.id, item_id: item.id)
