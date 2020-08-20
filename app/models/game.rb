043508951249106fc993a8a26ee43498ecafed17
kevinpark07 <kevin.park07@yahoo.com>
2020-08-20 10:58:20 -0400

    fixed login

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,33 +14,43 @@ class Game < ActiveRecord::Base
     def start
         system("clear")
         # puts @@pastel.red(@@font.write("Hogwarts School of Witchcraft & Wizardry"))
-        start_menu
         puts "Welcome to Hogwarts! My name is Argus Filch. There have been many curious and dangerous occurences in these halls."
-        # existing = @@prompt.select("Have you played with us before?", %w(Yes No))
-        # if existing == "Yes" ? self.existing_user : self.new_user
+        start_menu
         puts "Welcome #{self.user.name}! Let's explore"
-        # self.navigation_menu
+        navigation_menu
         self.new_game
     end
 
     def start_menu
         #@@font name
         @@prompt.select("Welcome to the start menu.", per_page: 3) do |menu|
-            menu.choice "Create a New Log-in", -> { new_user }
-            menu.choice "Log-in", -> { existing_user }
+            menu.choice "Create a New Log-in", -> { User.new_user }
+            menu.choice "Log-in", -> { User.existing_user }
             menu.choice "Exit", -> { exit }
         end
+    end
+
+    def new_game
+        system("clear")
+        items = self.choose_items
+        spells = self.choose_spells
+        self.user.get_ready(items, spells)
+        self.navigation_menu
+        # result = @@prompt.select("Would you like to review your selections or continue?", %w(Review Continue))
+        # list table of spells/items
+        # @@prompt.select("Congratulations, it looks like you've chosen wisely. Time to choose your objective!", %w(Ok!))
+        # choice = choose_scenario
+        # start_scenario(choice)
     end
     
 
     
     def navigation_menu
         #@@font name
-        @@prompt.select(@@pastel.green("Welcome, what do you want to do?"), per_page: 6) do |menu|
+        @@prompt.select(@@pastel.green("Welcome, what do you want to do?"), per_page: 5) do |menu|
             menu.choice "View Current Items", -> { view_items }
-            menu.choice "Change Current Items", -> { change_items }
             menu.choice "View Current Spells", -> { view_spells }
-            menu.choice "Change Current Spells", -> { change_spells }
+            menu.choice "Change Current Items/Spells", -> { change_items_spells }
             menu.choice "Choose Scenario", -> { choose_scenario }
             menu.choice "Exit", -> { exit }
         end
@@ -65,17 +75,22 @@ class Game < ActiveRecord::Base
         end
     end
 
-    def change_items
-        result = @@prompt.select("Would you like to change items?", %w[Yes No])
+    def change_items_spells
+        result = @@prompt.select("Would you like to change your items and spells?", %w[Yes No])
         if result == "Yes"
             items = UserItem.all.find_all {|ui| ui.user_id == self.user.id}
-            items[0].delete
-            items[1].delete
+            spells = UserSpell.all.find_all {|us| us.user_id == self.user.id}
+            binding.pry
+            items[0].destroy
+            items[1].destroy
+            spells[0].destroy
+            spells[1].destroy
+            binding.pry
+            self.new_game
         elsif result == "No"
             self.navigation_menu
         end
     end
-
 
     def view_spells
         # item_box = TTY::Box.frame 
@@ -95,73 +110,7 @@ class Game < ActiveRecord::Base
             self.navigation_menu
         end
     end
-
-
-
-    def existing_user
-        username = @@prompt.ask("Please enter you username >>")
-        password = @@prompt.mask("Please enter your password >>")
-        user = User.all.find_by(username: username, password: password)
-        if user == nil
-            puts "Sorry, your name and/or password was not found. Please try again."
-            self.existing_user 
-        else
-            user.game = self
-        end
-        self.navigation_menu
-    end
-
-    def new_user #updated
-        system("clear")
-        create_new_user
-        name = @@prompt.ask("What is your name, young wizard? >>", echo: true)
-        self.user.name = name
-        puts "Hi #{self.user.name}! Interesting name. A few more questions..."
-        bloods = %w[pure-blood half-blood muggle unkown]
-        blood = @@prompt.select("What is your blood type?", bloods)
-        user.update(bloodStatus: blood)
-        houses = %w[Gryffindor Slytherin Ravenclaw Hufflepuff]
-        house = @@prompt.select("What house do you belong to?", houses)
-        user.update(house: house)
-        user
-    end
-
-    def create_new_user #new
-        # system("clear")
-        create_username
-        password = @@prompt.mask("Please enter a Password:")
-        password_check = @@prompt.mask("Please Re-enter Your Password:")
-        if password_check != password
-            puts "We're sorry, but you passwords don't seem to match. Please try again"
-            password = @@prompt.ask("Please enter a Password:")
-            password_check = @@prompt.ask("Please Re-enter Your Password:")
-            user.update(password: password_check)
-        else
-            user.update(password: password_check)
-        end
-    end
-
-    def create_username
-        username = @@prompt.ask("Please enter a Username >>")
-        user = User.create(username: username)
-        user.game = self
-    end
-
-
     
-    def new_game
-        system("clear")
-        items = self.choose_items
-        spells = self.choose_spells
-        self.user.get_ready(items, spells)
-        self.navigation_menu
-        # result = @@prompt.select("Would you like to review your selections or continue?", %w(Review Continue))
-        # list table of spells/items
-        # @@prompt.select("Congratulations, it looks like you've chosen wisely. Time to choose your objective!", %w(Ok!))
-        # choice = choose_scenario
-        # start_scenario(choice)
-    end
-
 
 
     def choose_items
@@ -202,35 +151,5 @@ class Game < ActiveRecord::Base
         end
     end
 
-    # def review_selection
-    #     choice = %w[Items Spells Exit]
-    #     result = @@prompt.select("Please select one to review.", choice)
-    #     if result == "Items"
-    #         result = @@prompt.select("Would you like to get new items?", %w[Yes No])
-    #         if result == "Yes"
-    #             items = User_items.all.find_by(user_id: self.user.id)
-    #             items.destroy
-    #             self.choose_items
-    #             self.review_selection
-    #         else
-    #             self.review_selection
-    #         end
-    #     elsif result == "Spells"
-    #         result = prompt.select("Would you like to learn new spells?", %w[Yes No])
-    #         if result == "Yes"
-    #             spells = User_spells.all.find_by(user_id: self.user.id)
-    #             spells.destroy
-    #             self.choose_spells
-    #             self.review_selection
-    #         else
-    #             self.review_selection
-    #         end
-    #     end
-    # end
-
                 
 end
-
-
-
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class User < ActiveRecord::Base
     has_many :user_scenarios
     has_many :user_spells
@@ -7,23 +9,101 @@ class User < ActiveRecord::Base
     has_many :items, through: :user_items
     belongs_to :game
 
-    # def pick_item(item)
-    #     UserItem.create(user_id: self.id, item_id: item.id)
-    # end
+    @@prompt = TTY::Prompt.new(active_color: :blue)
 
-    # def pick_spell(spell)
-    #     UserSpell.create(user_id: self.id, spell_id: spell.id)
-    # end
+    def self.existing_user
+        username = @@prompt.ask("Please enter you username:")
+        exist_user = User.find_by(username: username)
+        if exist_user
+            password = @@prompt.mask("Please enter your password:")
+            count = 0
+            while exist_user.password != password
+                puts "Wrong password. Please try again."
+                password = @@prompt.mask("Please enter your password:")
+                count += 1
+                if count == 2
+                    puts "Sorry, account has been locked."
+                    return false
+                end
+            end
+        else
+            puts "#{username} not found"
+            return false
+        end
+    end
 
-    # def options
-    #     self.items.each_with_index do |item, index|
-    #         puts "#{index +1}. #{item.name}: #{item.description}"
-    #     end
 
-    #     self.spells.each_with_index do |s,i|
-    #         puts "#{i +1}. #{s.spell}: #{s.effect}"
-    #     end
-    # end
+    def self.new_user
+        system("clear")
+        username = self.create_username
+        password = self.create_password
+        game = Game.last
+        user = User.create(username: username, password: password)
+        game.update(user: user)
+        binding.pry
+        user.user_info
+    end
+
+    def self.create_username
+        username = @@prompt.ask("Please create a new Username:")
+        if User.find_by(username: username) == nil
+            username
+        else username == User.find_by(username: username).username
+            puts "Username already exists."
+            self.create_username
+        end
+    end
+
+    def self.create_password
+        password = @@prompt.mask("Please enter a Password:")
+        password_check = @@prompt.mask("Please Re-enter Your Password:")
+        if password_check != password
+            puts "We're sorry, but your passwords don't seem to match. Please try again"
+            self.create_password
+        else
+            password_check 
+        end
+    end
+
+    def user_info
+        name = self.add_name
+        house = self.add_house
+        blood = self.add_blood
+        self.update(name: name, house: house, bloodStatus: blood)
+    end
+
+    def add_house
+        puts "Hi #{self.name}! Interesting name. A few more questions..."
+        houses = %w[Gryffindor Slytherin Ravenclaw Hufflepuff]
+        house = @@prompt.select("What house do you belong to?", houses)
+    end
+
+    def add_blood
+        bloods = %w[pure-blood half-blood muggle unkown]
+        blood = @@prompt.select("What is your blood type?", bloods)
+    end
+
+    def add_name
+        name = @@prompt.ask("What is your name, young wizard?", echo: true)
+    end
+
+
+
+
+
+
+
+
+        # user = User.all.find_by(username: username, password: password)
+        # if user == nil
+        #     puts "Sorry, your name and/or password was not found. Please try again."
+        #     self.existing_user 
+        # else
+        #     user.game = self
+        # end
+        # self.navigation_menu
+
+
 
     def spell_names
         self.spells.map {|spell| spell.spell_name}
